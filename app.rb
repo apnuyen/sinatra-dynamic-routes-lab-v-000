2819d581bb177d20553c707477147991d24a1d4d
An Nuyen <annuyen@gmail.com>
2018-02-27 03:05:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,14 +13,27 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @num = params[:number].to_i 
-    "#{params[:phrase]} "* @num 
+    @num = params[:number].to_i
+    "#{params[:phrase]}" * @num
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-
+    @sentence = params.values.join(" ")
+    "#{@sentence}."
   end
 
   get '/:operation/:number1/:number2' do
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+    case params[:operation]
+    when 'add'
+      "#{num1 + num2}"
+    when 'subtract'
+      "#{num1 - num2}"
+    when 'multiply'
+      "#{num1*num2}"
+    when 'divide'
+      "#{num1/num2}"
+    end
   end
 end
